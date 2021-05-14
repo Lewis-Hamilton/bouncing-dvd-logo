1a25c8b08657a82450807fce1808c31342a959f8
unknown <hamiltonjlewis@gmail.com>
2021-05-13 23:34:56 -0500

    optimized

--- crazy.js
+++ crazy.js
@@ -4,7 +4,7 @@ let y;
 let xspeed;
 let yspeed;
 
-let logo
+let logos = []
 let dvd;
 let imageWidth;
 let imageHeight;
@@ -17,10 +17,23 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  logo = [1, 2, 3, 4]
   for (var i = 0; i < 4; i++) {
-    logo[i] = new bounce();
+    logos[i] = new bounce();
   }
+}
+
+function draw() {
+  background(0);
+  for (let logo of logos) {
+    logo.move();
+    logo.show();
+  }
+}
+
+function pickColor() {
+    r = random(100, 256);
+    g = random(100, 256);
+    b = random(100, 256);
 }
   
 class bounce {
@@ -36,40 +49,30 @@ class bounce {
   move() {
     this.x = this.x + this.xspeed;
     this.y = this.y + this.yspeed;
-    tint(r, g, b);
-    image(dvd, this.x, this.y, this.imageWidth, this.imageHeight);
 
     if (this.x + this.imageWidth >= width) {
-    this.xspeed = -this.xspeed;
+      this.xspeed = -this.xspeed;
       this.x = width - this.imageWidth;
       pickColor();
-  } else if (this.x <= 0) {
-    this.xspeed = -this.xspeed;
+    } else if (this.x <= 0) {
+      this.xspeed = -this.xspeed;
       this.x = 0;
       pickColor();
+    }
+
+    if (this.y + this.imageHeight >= height) {
+      this.yspeed = -this.yspeed;
+      this.y = height - this.imageHeight;
+      pickColor();
+    } else if (this.y <= 0) {
+      this.yspeed = -this.yspeed;
+      this.y = 0;
+      pickColor();
+    }
   }
 
-  if (this.y + this.imageHeight >= height) {
-    this.yspeed = -this.yspeed;
-    this.y = height -this.imageHeight;
-    pickColor();
-  } else if (this.y <= 0) {
-    this.yspeed = -this.yspeed;
-    this.y = 0;
-    pickColor();
-  }
-  }
-}
-
-function pickColor() {
-  r = random(100, 256);
-  g = random(100, 256);
-  b = random(100, 256);
-}
-
-function draw() {
-  background(0);
-  for (var i = 0; i < 4; i++) {
-    logo[i].move();
+  show() {
+    tint(r, g, b);
+    image(dvd, this.x, this.y, this.imageWidth, this.imageHeight);
   }
 }
